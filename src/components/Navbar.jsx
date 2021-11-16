557b4ee7a72a24ad2791671af0c0385cada821c6
martinharts <89169036+martinharts@users.noreply.github.com>
2021-11-15 20:14:36 -0500

    created react app from application. installed dependencies needed. created Navbar component and working on frontend

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button, Menu, Typography, Avatar} from 'antd';
+import {Link } from 'react-router-dom';
+import {HomeOutLined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar

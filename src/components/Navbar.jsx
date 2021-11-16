c0ad5c8fb4022c26c8132d59a33e5cdabf6de9de
martinharts <89169036+martinharts@users.noreply.github.com>
2021-11-15 20:43:28 -0500

    created navbar component and imported to app tested navbar and dependencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import {Button, Menu, Typography, Avatar} from 'antd';
 import {Link } from 'react-router-dom';
 import {HomeOutLined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import icon from "../images/cryptocurrency.png";
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={icon} size="large" />
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">Crypton</Link>
+                </Typography.Title>
+                {/* <Button className="menu-control-container">
+
+                </Button> */}
+            </div>
             
         </div>
     )

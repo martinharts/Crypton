6ad12152b9b6a368ab1bfea5879d35f99639e9d6
martinharts <89169036+martinharts@users.noreply.github.com>
2021-11-16 21:13:09 -0500

    added css styling to app. created links to homepage, exchanges, currencies, and news.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button, Menu, Typography, Avatar} from 'antd';
 import {Link } from 'react-router-dom';
-import {HomeOutLined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from "../images/cryptocurrency.png";
 
 const Navbar = () => {
@@ -12,10 +12,21 @@ const Navbar = () => {
                 <Typography.Title level={2} className="logo">
                     <Link to="/">Crypton</Link>
                 </Typography.Title>
-                {/* <Button className="menu-control-container">
-
-                </Button> */}
             </div>
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined/>}>
+                    <Link to ="/">Home</Link>
+                </Menu.Item>  
+                <Menu.Item icon={<FundOutlined/>}>
+                    <Link to ="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>  
+                <Menu.Item icon={<MoneyCollectOutlined/>}>
+                    <Link to ="/exchanges">Exchanges</Link>
+                </Menu.Item>  
+                <Menu.Item icon={<BulbOutlined/>}>
+                    <Link to ="/news">News</Link>
+                </Menu.Item>  
+            </Menu>
             
         </div>
     )
